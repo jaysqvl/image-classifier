02e765132c40b3e77b5ee4a98e2bd9ccf524ab83
jsqvl <jaysqvl@gmail.com>
2023-05-09 23:42:01 -0700

    Set-up file back-end file structure

---
+++ backend/app/routes.py
@@ -0,0 +1,9 @@
+#############################################
+## Flask Controller Back-end               ##
+## 1. Processes the image                  ##
+## 2. Loads the pre-trained Pytorch model  ##
+## 3. Classifies the image                 ##
+## 4. Sends the classification result back ##
+##      to the frontend including any      ##
+##      visualizations/data                ##
+#############################################
